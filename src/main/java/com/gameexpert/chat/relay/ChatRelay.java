6e77c980e3d7438febdd259532f92a617ff9f5e3
Kim Woojin <bakak112@naver.com>
2026-09-21 12:15:59 +0900

    Lv 20. 멀티 서버

--- src/main/java/com/gameexpert/chat/relay/ChatRelay.java
+++ src/main/java/com/gameexpert/chat/relay/ChatRelay.java
@@ -1,5 +1,6 @@
 package com.gameexpert.chat.relay;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
@@ -22,10 +23,19 @@ public class ChatRelay implements MessageListener {
 
     public void publish(Long worldId, Object message) {
         // TODO Lv 20: worldId와 message를 JSON으로 묶어 채팅 채널에 발행합니다.
+        String jsonMessage = objectMapper.writeValueAsString(Map.of("worldId", worldId, "message", message));
+        redisTemplate.convertAndSend(CHANNEL, jsonMessage);
     }
 
     @Override
-    public void onMessage(Message message, byte[] pattern) {
+    public void onMessage(Message serializedMessage, byte[] pattern) {
         // TODO Lv 20: JSON에서 worldId와 message를 읽어 localChatSender.send()로 전달합니다.
+        String jsonMessage = new String(serializedMessage.getBody(), StandardCharsets.UTF_8);
+
+        JsonNode jsonNode = objectMapper.readTree(jsonMessage);
+        Long worldId = jsonNode.get("worldId").asLong();
+        Object message = jsonNode.get("message");
+
+        localChatSender.send(worldId, message);
     }
 }
